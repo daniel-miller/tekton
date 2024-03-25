d1c2726f354a03f2d4409ff18dee71c50d6aeaf5
Daniel Miller <daniel@insite.com>
2024-03-25 09:22:38 -0600

    Add lifetime to token request

---
+++ Source/Common.Sdk/ApiTokenRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace Common.Sdk
+{
+    public class ApiTokenRequest
+    {
+        public string Secret { get; set; }
+        public int? Lifetime { get; set; }
+
+        public ApiTokenRequest(string secret, int? lifetime)
+        {
+            Secret = secret;
+            Lifetime = lifetime;
+        }
+    }
+}
