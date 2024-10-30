378f0719e64bb639337c3d795808555a91f3eac4
Daniel Miller <daniel@insite.com>
2024-10-29 20:03:52 -0600

    Synchronize branch

---
+++ Source/Common/Exception/IExceptionMonitor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Common
+{
+    public interface IExceptionMonitor
+    {
+        PlatformOptions Platform { get; }
+        SentryOptions Sentry { get; }
+
+        void Error(Exception ex);
+    }
+}
---
+++ Source/Common/Exception/SentryOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace Common
+{
+    public class SentryOptions
+    {
+        public string Dsn { get; set; }
+        public bool Enabled { get; set; }
+        public string Log { get; set; }
+        public double Rate { get; set; }
+
+        public bool Disabled => !Enabled || !Valid;
+        public bool Valid => !string.IsNullOrEmpty(Dsn) && Rate > 0;
+    }
+}
