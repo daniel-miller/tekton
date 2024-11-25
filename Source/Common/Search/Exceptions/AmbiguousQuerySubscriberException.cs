eafc3eb11bd0e894377dff73e5d7c09cf0e76152
Daniel Miller <daniel@insite.com>
2024-11-25 14:24:26 -0700

    Improve the readability of the code for the query handler

---
+++ Source/Common/Search/Exceptions/AmbiguousQuerySubscriberException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Common.Search
+{
+    [Serializable]
+    public class AmbiguousQuerySubscriberException : Exception
+    {
+        public AmbiguousQuerySubscriberException(string name)
+            : base($"You cannot define multiple subscribers for the same query ({name}).")
+        {
+        }
+
+        protected AmbiguousQuerySubscriberException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+
+    [Serializable]
+    public class MissingQuerySubscriberException : Exception
+    {
+        public MissingQuerySubscriberException(string name)
+            : base($"There is no subscriber for this query ({name}).")
+        {
+        }
+
+        protected MissingQuerySubscriberException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
--- Source/Common/Search/Queries/QueryHandler.cs
+++ Source/Common/Search/Queries/QueryHandler.cs
@@ -5,32 +5,21 @@ using System.Linq;
 namespace Common.Search
 {
     /// <summary>
-    /// Implements a basic query queue. The purpose of the queue is to route queries to handler methods; validation of
-    /// a query itself is the responsibility of its handler.
+    /// Implements a basic queue for handling queries. The purpose of the queue is to route queries to handler methods; 
+    /// validation of a query itself is the responsibility of its subscriber.
     /// </summary>
     public class QueryHandler : IQueryHandler
     {
         private readonly Reflector _reflector = new Reflector();
 
         /// <summary>
-        /// A query's full class name is the key to find the method that handles it.
+        /// A query's full class name is the key to find the method that subscribes to it.
         /// </summary>
-        private readonly Dictionary<string, Delegate> _handlers = new Dictionary<string, Delegate>();
+        private readonly Dictionary<string, Delegate> _subscribers = new Dictionary<string, Delegate>();
 
         public QueryHandler()
         {
             
-        }
-
-        private TResult Execute<TResult>(IQuery<TResult> query, string @class)
-        {
-            if (_handlers.ContainsKey(@class))
-            {
-                var action = _handlers[@class];
-                return (TResult)action.DynamicInvoke(query);
-            }
-
-            return default;
         }
 
         public TResult Execute<TResult>(IQuery<TResult> query)
@@ -42,10 +31,20 @@ namespace Common.Search
         {
             var name = _reflector.GetClassName(typeof(TQuery));
 
-            if (_handlers.Any(x => x.Key == name))
-                throw new AmbiguousQueryHandlerException(name);
+            if (_subscribers.Any(x => x.Key == name))
+                throw new AmbiguousQuerySubscriberException(name);
 
-            _handlers.Add(name, handle);
+            _subscribers.Add(name, handle);
+        }
+
+        private TResult Execute<TResult>(IQuery<TResult> query, string @class)
+        {
+            if (!_subscribers.ContainsKey(@class))
+                throw new MissingQuerySubscriberException(@class);
+
+            var action = _subscribers[@class];
+
+            return (TResult)action.DynamicInvoke(query);
         }
     }
 }
